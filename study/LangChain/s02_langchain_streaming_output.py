eb5955e76787ca2ceea0d4dfb8882123599c89aa
pain-too <2024014456@nwafu.edu.cn>
2026-05-18 19:31:50 +0800

    项目整理

--- study/LangChain/s02_langchain_streaming_output.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.llms.tongyi import Tongyi
-
-model = Tongyi(model="qwen-max")
-
-res = model.stream(input="你好吗")
-for chunk in res:
-    print(chunk,end="",flush=True)
--- study/LangChain/s05_embedding_model.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.embeddings import DashScopeEmbeddings as Embed
-embed = Embed()
-#测试
-print(embed.embed_query("i hate u"))
-print(embed.embed_documents(
-    ["i hate u",
-     "f^^k you",
-     "f^^k you leli much"])
-)
